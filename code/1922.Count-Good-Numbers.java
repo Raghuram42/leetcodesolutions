7a8633dc9b0746112a966a3ffa71a8414944e938
Raghuram <voruganti.raghuram@gmail.com>
2021-07-04 21:27:36 +0530

    Update 1922.Count-Good-Numbers.java

--- code/1922.Count-Good-Numbers.java
+++ code/1922.Count-Good-Numbers.java
@@ -1,30 +1,31 @@
 class Solution {
-    HashMap<Character, List<Character>> map;
-    int mod;
-    int[][] dp;
-    public int countVowelPermutation(int n) {
-        map = new HashMap<>();
-        mod = 1000000007;
-        dp = new int[n+1][26];
-        map.put('f', Arrays.asList('a','e','i','o','u'));
-        map.put('a', Arrays.asList('e'));
-        map.put('e', Arrays.asList('i', 'a'));
-        map.put('i', Arrays.asList('a','e','o','u'));
-        map.put('o', Arrays.asList('i', 'u'));
-        map.put('u', Arrays.asList('a'));
+    int mod = 1000000007;
+    public int countGoodNumbers(long n) {
         
-        return solve(n, 'f');
+        
+        long odd = n/2;
+        long even = n%2 == 1 ? (n/2)+1: n/2;
+        long res = getAns(odd, 4);
+        res = (res * getAns(even, 5))%mod;
+        
+        return (int)(res%mod);
+        
+        
+        
+    }
+    public long getAns(long val, int div){
+        return ((logPow(val, div))%mod);
     }
     
-    public int solve(int n, Character cur){
-        if(n==0)
+    public long logPow(long val, int div){
+        
+        if(val == 0)
             return 1;
-        if(dp[n][cur-'a'] != 0)
-            return dp[n][cur-'a'];
-        int ans = 0;
-        for(Character next: map.get(cur))
-            ans = (ans+solve(n-1, next))%mod;
-            
-        return dp[n][cur-'a'] = ans%mod;
+        long res = (logPow(val/2, div))%mod;
+        long evenres = (res*res)%mod;
+        if(val%2 == 0)
+            return evenres;
+        
+        return (evenres*div)%mod;
     }
 }
