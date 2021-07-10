7dfc1cf28e1cd24d603f42c982ba7ec67d6e6a61
Raghuram <voruganti.raghuram@gmail.com>
2021-07-10 21:21:21 +0530

    Create 5792.Count-Square-Sum-Triples.java

---
+++ 5792.Count-Square-Sum-Triples.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countTriples(int n) {
+        int res = 0;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                for(int k=1;k<=n;k++)
+                    if(i*i + j*j == k*k)
+                        res++;
+            }
+        }
+        return res;
+    }
+}
