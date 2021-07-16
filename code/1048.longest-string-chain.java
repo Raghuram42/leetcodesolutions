e4a53a3e5956f43d191a3bb22313f3ec059a1846
Raghuram <voruganti.raghuram@gmail.com>
2021-07-17 00:06:40 +0530

    Update 1048.longest-string-chain.java

--- code/1048.longest-string-chain.java
+++ code/1048.longest-string-chain.java
@@ -1,4 +1,116 @@
 class Solution {
+    boolean[] vis;
+    int[] dp;
+    public int longestStrChain(String[] words) {
+        int n = words.length;
+        vis = new boolean[n];
+        dp = new int[n];
+        Arrays.fill(dp, -1);
+        int res = 0;
+        
+        for(int i=0;i<n;i++)
+        {
+            res = Math.max(res, solve(words, i));
+        }
+        // res++;
+        
+        return res;
+    }
+    
+    
+    public int solve(String[] words, int i){
+        if(i >= words.length)
+            return 0;
+        
+        if(dp[i] != -1)
+            return dp[i];
+        int res = 0;
+        int n = words.length;
+        vis[i] = true;
+        for(int j=0;j<n;j++){
+                // System.out.println(words[i] +" out "+ words[j]);
+            if(!vis[j] && i != j && same(words[i], words[j])){
+                // System.out.println(words[i] +" "+ words[j]);
+                // vis[j] = true;
+            res = Math.max(res, solve(words, j));
+                // vis[j] = false;
+            }
+            
+        }
+        
+        vis[i] = false;
+        return dp[i] = res+1;
+    }
+    
+    public boolean same(String w1, String w2){
+        if(w1.length() == w2.length())
+            return false;
+        int st = 0;
+        int end = 0;
+        int n1 = w1.length();
+        int n2 = w2.length();
+        int diff = 0;
+        while(st < n1 || end < n2){
+            if(st<n1 && end<n2 && w1.charAt(st) == w2.charAt(end) ){
+                st++;
+                end++;
+            }else{
+                diff++;
+                end++;
+            }
+            
+            if(diff > 2)
+                return false;
+        }
+        return diff == 1;
+    }
+    
+}
+
+//solve(words, i=0)
+/*
+
+{
+    > len
+        return 0;
+    res = 1;
+    for(j=0 to n)
+    {
+        vis[j] = t;
+        if(i != j && same(i, j))
+          max(res, solve(words, j));
+        vis[j] = f;
+    }
+        ret rs;
+}
+
+same(t1, t2){
+    int f= 0;
+    int s=0;
+    int diff = 0;
+    while(f<n1 ||  s<n2){
+        if(f<n1 &&  s<n2 && equal)
+        {
+            f++;
+            s++;
+        }else{
+            diff++;
+            s++;
+        }
+        
+        if(diff > 1)
+            break;
+    }
+    
+    ret dif == 1; 
+}
+
+ba b
+*/
+/*
+Old solution 
+
+class Solution {
     int[][][] dp;
     public int longestStrChain(String[] words) {
         HashMap<Integer, List<String>> lengthWise = new HashMap<Integer, List<String>>();
@@ -79,3 +191,4 @@ class Solution {
         
     }
 }
+*/
