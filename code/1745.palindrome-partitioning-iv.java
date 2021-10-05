95e18ef14c8431b82d01e6f6a2c6d3b892ff1c5e
Raghuram <voruganti.raghuram@gmail.com>
2021-10-05 20:45:56 +0530

    Update 1745.palindrome-partitioning-iv.java

--- code/1745.palindrome-partitioning-iv.java
+++ code/1745.palindrome-partitioning-iv.java
@@ -3,25 +3,29 @@ class Solution {
         int n = s.length();
      int[][] dp = new int[n][n];   
         
-     for(int i=0;i<n;i++)
+     for(int i=0;i<n;i++){
          dp[i][i] = 1;
-     for(int i=1;i<n;i++)
-         if(s.charAt(i) == s.charAt(i-1))
+         if(i >0 && s.charAt(i) == s.charAt(i-1))
              dp[i-1][i] = 1;
+     }
+     
+         
         
       for(int i=2;i<n;i++){
-          for(int j=0;j<n-i;j++){
-              int p = j+i;
-              if(s.charAt(j) == s.charAt(p) && dp[j+1][p-1] == 1)
-                  dp[j][p] = 1;
+          for(int j=0;j<i;j++){
+              
+              if(s.charAt(j) == s.charAt(i) && dp[j+1][i-1] == 1)
+                  dp[j][i] = 1;
           }
       }
         
         
         for(int i=0;i<n;i++){
+            if(dp[0][i] == 1){
             for(int j=0;j<n-1;j++){
-                if(dp[0][i] == 1 && dp[i+1][j] == 1 && dp[j+1][n-1] == 1 )
+                if(dp[i+1][j] == 1 && dp[j+1][n-1] == 1 )
                     return true;
+            }
             }
         }
         return false;
