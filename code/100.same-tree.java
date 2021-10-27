cf40d3d411e5418c3e8920b26e2877908c66bfe3
Raghuram <voruganti.raghuram@gmail.com>
2021-10-27 21:19:36 +0530

    Update 100.same-tree.java

--- code/100.same-tree.java
+++ code/100.same-tree.java
@@ -20,7 +20,6 @@ class Solution {
         if(p == null && q != null || p != null && q == null)
             return false;
         
-        
         if(p.val == q.val)
             return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         
