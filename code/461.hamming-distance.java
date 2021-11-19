5f0ccd444a8ffaa706958f8e61a3482abc08fa18
Raghuram <voruganti.raghuram@gmail.com>
2021-11-19 16:38:13 +0530

    Update 461.hamming-distance.java

--- code/461.hamming-distance.java
+++ code/461.hamming-distance.java
@@ -1,13 +1,15 @@
 class Solution {
     public int hammingDistance(int x, int y) {
+        int ans = 0;
         
-        //System.out.print
-        int n =  (x | y) & ~(x & y) ;
-          int count = 0; 
-        while (n > 0) { 
-            n &= (n - 1); 
-            count++; 
-        } 
-        return count; 
+        for(int i=0;i<32;i++){
+            if((x&1) != (y&1))
+                ans++;
+            
+            x = x>>1;
+            y = y>>1;
+        }
+        
+        return ans;
     }
 }
