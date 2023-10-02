59b6beede29cb13cd175ee30e81b4ce9397dc2b2
Raghuram <voruganti.raghuram@gmail.com>
2023-10-01 20:45:43 -0400

    Time: 29 ms (76.74%), Space: 59.9 MB (46.01%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,42 +1,40 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        Deque<int[]> q = new LinkedList<int[]>();
-        
+        Deque<Integer> q = new LinkedList<>();
         
         int i = 0;
         int n = nums.length;
-         while(i<k){
-                if(q.isEmpty() || nums[i] < nums[q.peekLast()[1]])
-                q.addLast(new int[]{nums[i], i});
+        
+        while(i<k){
+            if(q.isEmpty() || nums[i] < nums[q.peekLast()])
+                q.addLast(i);
             else{
-                while(!q.isEmpty() && nums[i] >= nums[q.peekLast()[1]])
+                
+                while(!q.isEmpty() && nums[i] >= nums[q.peekLast()])
                     q.pollLast();
-                q.addLast(new int[]{nums[i], i});
+                
+                q.addLast(i);
             }
             i++;
         }
         
-        
         int[] res = new int[n-k+1];
         while(i<n){
-            res[i-k] = nums[q.peekFirst()[1]];
+            res[i-k] = nums[q.peekFirst()];
             
-            if(!q.isEmpty() && q.peekFirst()[1] <= i-k)
+            if(q.peekFirst() <= i-k)
                 q.pollFirst();
             
             
-            while(!q.isEmpty() && (nums[q.peekLast()[1]] <= nums[i]))
-                    q.pollLast();
-                
-                // if(q.isEmpty())
-                    q.addLast(new int[]{nums[i], i});
-          
+            while(!q.isEmpty() && nums[i] >= nums[q.peekLast()])
+                q.pollLast();
             
-            
+            q.addLast(i);
             i++;
         }
         
-        res[i-k] = nums[q.peekFirst()[1]];
+        res[i-k] = nums[q.peekFirst()];
+        
         return res;
     }
 }
