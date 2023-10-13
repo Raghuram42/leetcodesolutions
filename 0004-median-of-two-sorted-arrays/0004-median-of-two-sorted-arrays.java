be4465375a8274dbf813d975e0749ef708dcf78d
Raghuram <voruganti.raghuram@gmail.com>
2023-10-12 20:08:59 -0400

    Time: 1 ms (100.00%), Space: 44.8 MB (16.64%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,20 +1,23 @@
 class Solution {
-    public double findMedianSortedArrays(int[] A, int[] B) {
-       int na = A.length, nb = B.length;
-        int n = na + nb;
-        if ((na + nb) % 2 == 1) {
-            return s(A, B, n / 2, 0, na - 1, 0, nb - 1);
-        } else {
-            return (double)(s(A, B, n / 2, 0, na - 1, 0, nb - 1) + s(A, B, n / 2 - 1, 0, na - 1, 0, nb - 1)) / 2;
-        }
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        
+        
+        int t = m+n;
+        
+        
+        if((m+n)%2 == 1)
+            return 0d+s(nums1, nums2, t/2, 0, m-1, 0, n-1);
+        
+        return (s(nums1, nums2, t/2, 0, m-1, 0, n-1)+s(nums1, nums2, (t/2)-1, 0, m-1, 0, n-1))/2d;
     }
     
-    
     public int s(int[] n1, int[] n2, int k, int as, int ae, int bs, int be){
-        if( ae< as)
+        if(as > ae)
             return n2[k-as];
         
-        if(be < bs)
+        if(bs > be)
             return n1[k-bs];
         
         int aI = (as+ae)/2;
@@ -35,35 +38,3 @@ class Solution {
         }
     }
 }
-/*
-    
-     public int s(int[] n1, int[] n2, int k, int as, int ae, int bs, int be) {
-     
-        if (ae < as) {
-            return n2[k - as];
-        }
-        if (be < bs) {
-            return n1[k - bs];
-        }
-        
-       
-        int aI = (as + ae) / 2, bI = (bs + be) / 2;
-        int aValue = n1[aI], bValue = n2[bI];
-        
-        // If k is in the right half of A + B, remove the smaller left half.
-        if (aI + bI < k) { 
-            if (aValue > bValue) {
-                return s(n1, n2, k, as, ae, bI + 1, be);
-            } else {
-                return s(n1, n2, k, aI + 1, ae, bs, be);
-            }
-        }
-        // Otherwise, remove the larger right half. 
-        else { 
-            if (aValue > bValue) {
-                return s(n1, n2, k, as, aI - 1, bs, be);
-            } else {
-                return s(n1, n2, k, as, ae, bs, bI - 1);       
-            }
- }
-*/
