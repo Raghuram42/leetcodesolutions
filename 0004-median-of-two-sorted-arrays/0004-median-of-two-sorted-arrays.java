f606b6481cfa724f6b1d51f32e4e06abfac2c8ad
Raghuram <voruganti.raghuram@gmail.com>
2023-10-12 20:06:06 -0400

    Time: 1 ms (100.00%), Space: 44.7 MB (28.72%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,48 +1,69 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int n = nums2.length;
-        
-        int mIndex = (m+n)%2 == 0 ? ((m+n)/2) : ((m+n)/2);
-        boolean odd = ((m+n)%2) == 1;
-        int lenC =0;
-        int i = 0;
-        int j = 0;
-        
-        int[] q = new int[2];
-        int k = 0;
-        while(i<m || j<n){
-            
-            if(i<m && j < n){
-            if(nums1[i] <= nums2[j]){
-                q[k%2] = nums1[i];
-                i++;
-                k++;
-            }
-            else{
-                q[k%2] = nums2[j];
-                k++;
-                j++;
-            }
-            }
-            else if(i<m){
-                    q[k%2] = nums1[i];
-                i++;
-                k++;
-                }else{
-                    q[k%2] = nums2[j];
-                k++;
-                j++;
-                }
-            // System.out.println(odd+" "+Arrays.toString(q)+" "+mIndex+" "+lenC);
-            if(lenC == mIndex){
-                if(odd)
-                    return (double)q[(k-1)%2];
-                return (q[0]+0d+q[1])/2;
-            }
-            
-            lenC++;   
+    public double findMedianSortedArrays(int[] A, int[] B) {
+       int na = A.length, nb = B.length;
+        int n = na + nb;
+        if ((na + nb) % 2 == 1) {
+            return s(A, B, n / 2, 0, na - 1, 0, nb - 1);
+        } else {
+            return (double)(s(A, B, n / 2, 0, na - 1, 0, nb - 1) + s(A, B, n / 2 - 1, 0, na - 1, 0, nb - 1)) / 2;
         }
-        return -1d;
+    }
+    
+    
+    public int s(int[] n1, int[] n2, int k, int as, int ae, int bs, int be){
+        if( ae< as)
+            return n2[k-as];
+        
+        if(be < bs)
+            return n1[k-bs];
+        
+        int aI = (as+ae)/2;
+        int bI = (bs+be)/2;
+        
+        if(aI+bI < k){
+        if(n1[aI] > n2[bI])
+            return s(n1, n2, k, as, ae, bI+1, be);
+        else {
+                return s(n1, n2, k, aI + 1, ae, bs, be);
+            }
+        }else{
+        if(n1[aI] > n2[bI])
+            return s(n1, n2, k, as, aI-1, bs, be);
+         else {
+                return s(n1, n2, k, as, ae, bs, bI - 1);       
+            }
+        }
     }
 }
+/*
+    
+     public int s(int[] n1, int[] n2, int k, int as, int ae, int bs, int be) {
+     
+        if (ae < as) {
+            return n2[k - as];
+        }
+        if (be < bs) {
+            return n1[k - bs];
+        }
+        
+       
+        int aI = (as + ae) / 2, bI = (bs + be) / 2;
+        int aValue = n1[aI], bValue = n2[bI];
+        
+        // If k is in the right half of A + B, remove the smaller left half.
+        if (aI + bI < k) { 
+            if (aValue > bValue) {
+                return s(n1, n2, k, as, ae, bI + 1, be);
+            } else {
+                return s(n1, n2, k, aI + 1, ae, bs, be);
+            }
+        }
+        // Otherwise, remove the larger right half. 
+        else { 
+            if (aValue > bValue) {
+                return s(n1, n2, k, as, aI - 1, bs, be);
+            } else {
+                return s(n1, n2, k, as, ae, bs, bI - 1);       
+            }
+ }
+*/
