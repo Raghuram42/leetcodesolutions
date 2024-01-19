b200e22f86b71a3f14110c8019a21400425dc62d
Raghuram <voruganti.raghuram@gmail.com>
2024-01-18 23:15:19 -0500

    Time: 14 ms (17.12%), Space: 58.5 MB (19.91%) - LeetHub

--- 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.java
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.java
@@ -1,29 +1,36 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
         int n = nums.length;
-        int[] map = new int[32];
-        int[] map2 = new int[32];
-        setBinary(k, map2);
-        int i = 0;
-        for(int num: nums)
-            setBinary(num, map);
+        
+        // Initialize arrays to store binary representations of 'k' and each 'num' in 'nums'
+        int[] targetBinary = new int[32];
+        int[] numBinaries = new int[32];
+        
+        // Convert 'k' and each 'num' to binary representation
+        setBinary(k, targetBinary);
+        for (int num : nums)
+            setBinary(num, numBinaries);
         
         int ans = 0;
-        for(i=0;i<32;i++){
-            if(map2[i] != map[i]%2)
+        
+        // Compare the binary representations and count the differing bits
+        for (int i = 0; i < 32; i++) {
+            if (targetBinary[i] != numBinaries[i] % 2) {
                 ans++;
+            }
         }
         
         return ans;
     }
     
-    public void setBinary(int n, int[] m){
-        int i=0;
+    // Helper function to convert an integer 'n' to binary representation and store it in array 'm'
+    public void setBinary(int n, int[] m) {
+        int i = 0;
         
-        while(i<32){
-            m[i] +=(n & (1<<i)) == (1<<i) ? 1: 0;
+        while (n > 0 && i < 32) {
+            m[i] += n & 1; // Add the least significant bit to the array
+            n >>= 1; // Right shift 'n' to process the next bit
             i++;
         }
-        
     }
 }
