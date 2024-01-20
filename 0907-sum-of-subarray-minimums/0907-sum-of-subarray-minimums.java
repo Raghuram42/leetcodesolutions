d834cc2f2cf2adfc82a3dc3f1edb3e27bd582186
Raghuram <voruganti.raghuram@gmail.com>
2024-01-20 00:50:44 -0500

    Time: 66 ms (26.87%), Space: 51 MB (20.48%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -1,38 +1,37 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        Stack<Integer> stk = new Stack<>();
-        
-        int i = 0;
+        Stack<Integer> leftStack = new Stack<>();
+        Stack<Integer> rightStack = new Stack<>();
         int n = arr.length;
         int[] left = new int[n];
-        while(i<n){
-              while(!stk.isEmpty() && arr[i] <= arr[stk.peek()])
-                stk.pop();
-            left[i] = stk.isEmpty() ? i : i-stk.peek()-1;
-         
-            
-            stk.push(i);
-            i++;
+        int[] right = new int[n];
+        
+        // Calculate left boundaries for each element
+        for (int i = 0; i < n; i++) {
+            while (!leftStack.isEmpty() && arr[i] <= arr[leftStack.peek()]) {
+                leftStack.pop();
+            }
+            left[i] = leftStack.isEmpty() ? i + 1 : i - leftStack.peek();
+            leftStack.push(i);
         }
         
-        stk = new Stack<>();
-        // System.out.println(Arrays.toString(left));
-        i = n-1;
+        // Calculate right boundaries for each element
+        for (int i = n - 1; i >= 0; i--) {
+            while (!rightStack.isEmpty() && arr[i] < arr[rightStack.peek()]) {
+                rightStack.pop();
+            }
+            right[i] = rightStack.isEmpty() ? n - i : rightStack.peek() - i;
+            rightStack.push(i);
+        }
+        
         long ans = 0;
-        while(i>=0){
-            
-            while(!stk.isEmpty() && arr[i] < arr[stk.peek()])
-                stk.pop();
-            int curr = stk.isEmpty() ? n-i-1 : stk.peek()-1-i;
-            ans = mod(ans+mod(mod(mod(mod(mod(left[i]*curr)*arr[i]))+mod(left[i]+0l+curr+1)*arr[i])));
-            // System.out.println(curr+ " "+ans+" "+mod(mod(mod(left[i]*curr)+mod(left[i]+curr+1))*arr[i]));
-            stk.push(i);
-            i--;
-        }
-        return (int)ans;
+        int modValue = 1000000007;
         
-    }
-    public long mod(long v){
-        return (v%1000000007);
+        for (int i = 0; i < n; i++) {
+            ans += (long)arr[i] * left[i] * right[i];
+            ans %= modValue;
+        }
+        
+        return (int)ans;
     }
 }
