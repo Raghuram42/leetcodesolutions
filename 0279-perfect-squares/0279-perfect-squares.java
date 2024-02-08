964922840efaa8a68c62526f740d84a7fefa9698
Raghuram <voruganti.raghuram@gmail.com>
2024-02-07 22:40:13 -0500

    Time: 59 ms (45.01%), Space: 42.5 MB (72.46%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,26 +1,22 @@
 class Solution {
-    Integer[][] dp;
+    int[] dp;
     public int numSquares(int n) {
-        List<Integer> sq = new ArrayList<>();
-        
-        for(int next=1;next*next<=n;next++)
-            sq.add(next*next);
-        dp = new Integer[101][n+1];   
-        return s(0, sq, n);
+        dp = new int[n+1];
+        return solve(n);
     }
     
-    public int s(int i, List<Integer> sq, int sum){
-        if(sum == 0)
+    public int solve(int n){
+        if(n == 0)
             return 0;
-        if(sum < 0 || i >= sq.size())
-            return Integer.MAX_VALUE-1;
+        if(dp[n] != 0)
+            return dp[n];
+        int res = Integer.MAX_VALUE-1;
+        for(int i=1;i*i<=n;i++)
+            res = Math.min(res , 1+solve(n-i*i));
         
-        if(dp[i][sum] != null)
-            return dp[i][sum];
-        // System.out.println(i+" "+sq.get(i)+" "+sum);
-        int w = 1+s(i, sq, sum-sq.get(i));
-        int wout = s(i+1, sq, sum);
         
-        return dp[i][sum] = Math.min(w, wout);
+        return dp[n] = res;
+        
+        
     }
 }
