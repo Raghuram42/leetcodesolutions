cd8cbe95e109e0be2881ae3d1751bf1ae32615b6
Raghuram <voruganti.raghuram@gmail.com>
2024-02-24 13:44:58 -0500

    Time: 143 ms (20.59%), Space: 114.4 MB (28.43%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.java
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.java
@@ -1,56 +1,44 @@
 class Solution {
     public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
-        int m = meetings.length;
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b)->a[1]-b[1]);
         
-        boolean[] secretKnown = new boolean[n];
-        int t = 1;
-        int i = 0;
-        secretKnown[0] = true;
-        secretKnown[firstPerson] = true;
-        Arrays.sort(meetings, (a,b)->a[2]-b[2]);//Sort the inp according thier time
-        while(t<100000 && i<m){
-            HashMap<Integer, List<Integer>> graph = new HashMap<Integer, List<Integer>>();
+        HashMap<Integer, List<int[]>> g = new HashMap<>();
+        
+        for(int[] m : meetings){
+            if(!g.containsKey(m[0]))
+                g.put(m[0], new ArrayList<int[]>());
             
-            while(i<m && meetings[i][2] <= t){//Create a graph less than curr t time
+               if(!g.containsKey(m[1]))
+                g.put(m[1], new ArrayList<int[]>());
+            
+            g.get(m[0]).add(new int[]{m[1], m[2]});
+            g.get(m[1]).add(new int[]{m[0], m[2]});
+        }
+        
+        pq.offer(new int[]{0, 0});
+        if(!g.containsKey(0))
+            g.put(0, new ArrayList<int[]>());
+            
+            g.get(0).add(new int[]{firstPerson, 0});
+        
+         if(!g.containsKey(firstPerson))
+            g.put(firstPerson, new ArrayList<int[]>());
+            
+            g.get(firstPerson).add(new int[]{0, 0});
+        HashSet<Integer> vis = new HashSet<>();
+        int t = 0;
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
+            
+            if(!vis.contains(curr[0]) && curr[1] >= t){
+                vis.add(curr[0]);
+                for(int[] next: g.getOrDefault(curr[0], new ArrayList<int[]>()))
+                    pq.offer(new int[]{next[0], next[1]});
                 
-                if(!graph.containsKey(meetings[i][0]))
-                    graph.put(meetings[i][0], new ArrayList<Integer>());
-                
-                
-                if(!graph.containsKey(meetings[i][1]))
-                    graph.put(meetings[i][1], new ArrayList<Integer>());
-                
-                graph.get(meetings[i][0]).add(meetings[i][1]);
-                graph.get(meetings[i][1]).add(meetings[i][0]);
-                i++;
+                t = Math.max(t, curr[1]);
             }
-            boolean[] vis = new boolean[n];
-            for(int next: graph.keySet())
-                dfs(graph, secretKnown, next, vis);//Do dfs on connected graph components and update secretKnown accordingly
-            
-            t++;
-            
         }
-        List<Integer> res = new ArrayList<>();
         
-        for(i=0;i<n;i++)
-            if(secretKnown[i])
-                res.add(i);
-        
-        return res;
-    }
-    
-    public void dfs(HashMap<Integer, List<Integer>> g, boolean[] sec, int curr, boolean[] vis){
-        if(!sec[curr])
-            return;
-        if(vis[curr])
-            return;
-        vis[curr] = true;
-        for(int next: g.get(curr))
-        {
-            sec[next] = true;
-            if(!vis[next])
-                dfs(g, sec, next, vis);
-        }
+        return new ArrayList<Integer>(vis);
     }
 }
