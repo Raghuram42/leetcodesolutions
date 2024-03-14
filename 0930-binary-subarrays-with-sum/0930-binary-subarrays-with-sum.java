f0962083076850ff499fdd79d2ca7f8316221a90
Raghuram <voruganti.raghuram@gmail.com>
2024-03-13 22:13:01 -0400

    Time: 22 ms (42.75%), Space: 51.9 MB (15.45%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int totalCount = 0;
+        int currentSum = 0;
+        // {prefix: number of occurrence}
+        Map<Integer, Integer> freq = new HashMap<>(); // To store the frequency of prefix sums
+
+        for (int num : nums) {
+            currentSum += num;
+            if (currentSum == goal){
+                totalCount++;
+            }
+
+            // Check if there is any prefix sum that can be subtracted from the current sum to get the desired goal
+            if (freq.containsKey(currentSum - goal)){
+                totalCount += freq.get(currentSum - goal);
+            }
+
+            freq.put(currentSum, freq.getOrDefault(currentSum, 0) + 1);
+        }
+
+        return totalCount;
+    }
+}
